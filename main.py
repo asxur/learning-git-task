b10a355d974e5fa3120040d7e86965675d4b9f26
Szymon Stawicki <stavviol@gmail.com>
2022-07-21 20:28:50 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+shoppings = {"Piekarnia": ["chleb" , "bułka"],
+            "Warzywniak": ["Cebula", "sałata"]
+            } 
+for shop, itemsy in shoppings.items():
+    print(f"Ide do {shop.upper()} i kupuje tam {itemsy.upper()}")
+
+
+
